ae2526897bdef666fbcadb05b1ea6c494914aeba
Mostafa Soliman <mostafamoma.moma@gmail.com>
2019-09-26 17:06:29 +0200

    MainActivity xml

--- app/src/main/java/com/example/elite/arhub/MainActivity.java
+++ app/src/main/java/com/example/elite/arhub/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.elite.arhub;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
